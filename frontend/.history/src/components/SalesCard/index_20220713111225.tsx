bef264585339d3c950d8a5760df4c66009afe2f3
Vinicius9821 <vinicius-atilio@outlook.com>
2022-07-13 11:14:40 -0300

    Datepicker

--- frontend/.history/src/components/SalesCard/index_20220713111225.tsx
+++ frontend/.history/src/components/SalesCard/index_20220713111225.tsx
@@ -10,20 +10,10 @@ function SalesCard() {
                 <h2 className="dsmeta-sales-title">Vendas</h2>
                 <div>
                     <div className="dsmeta-form-control-container">
-                        <DatePicker
-                            selected={new Date()}
-                            onChange={(date: Date) => {}}
-                            className="dsmeta-form-control"
-                            dateFormat="dd/MM/yyyy"
-                        />
+                        <input className="dsmeta-form-control" type="text" />
                     </div>
                     <div className="dsmeta-form-control-container">
-                        <DatePicker
-                            selected={new Date()}
-                            onChange={(date: Date) => {}}
-                            className="dsmeta-form-control"
-                            dateFormat="dd/MM/yyyy"
-                        />
+                        <input className="dsmeta-form-control" type="text" />
                     </div>
                 </div>
 
